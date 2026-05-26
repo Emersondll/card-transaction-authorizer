35249a1a55fb2853e956e11f6226197ca2650e7e
Emerson Lima <emersondll@outlook.com>
2026-05-26 17:52:58 -0300

    test: add TransactionModelTest.java

---
+++ src/test/java/com/caju/transactionauthorizer/model/TransactionModelTest.java
@@ -0,0 +1,41 @@
+package com.caju.transactionauthorizer.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import com.caju.transactionauthorizer.model.TransactionModel;
+import java.math.BigDecimal;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TransactionModelTest {
+
+    private TransactionModel transactionModel;
+
+    @BeforeEach
+    public void setUp() {
+        transactionModel = new TransactionModel("123456", BigDecimal.valueOf(100), "5411", "Merchant Name");
+    }
+
+    @Test
+    public void testTransactionModelConstructor() {
+        assertEquals("123456", transactionModel.account());
+        assertEquals(BigDecimal.valueOf(100), transactionModel.totalAmount());
+        assertEquals("5411", transactionModel.mcc());
+        assertEquals("Merchant Name", transactionModel.merchant());
+    }
+
+    @Test
+    public void testTransactionModelEquals() {
+        TransactionModel sameTransaction = new TransactionModel("123456", BigDecimal.valueOf(100), "5411", "Merchant Name");
+        TransactionModel differentTransaction = new TransactionModel("654321", BigDecimal.valueOf(200), "1145", "Different Merchant");
+
+        assertEquals(transactionModel, sameTransaction);
+        assertNotEquals(transactionModel, differentTransaction);
+    }
+
+    @Test
+    public void testTransactionModelHashCode() {
+        TransactionModel sameTransaction = new TransactionModel("123456", BigDecimal.valueOf(100), "5411", "Merchant Name");
+
+        assertEquals(transactionModel.hashCode(), sameTransaction.hashCode());
+    }
+}
